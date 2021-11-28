c29e0f72b17a0ab275f13d3af02bebe68798eb2f
Ken Tien <kentien8888@gmail.com>
2021-11-27 19:37:16 -0600

    Add three more method to test the gas overfilling issue

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -1,5 +1,6 @@
 using CarNS;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace CarTests
 {
@@ -39,6 +40,23 @@ namespace CarTests
         {
             test_car.Drive(50);
             Assert.AreEqual(9, test_car.GasTankLevel, .001);
+        }
+
+        //TODO: gasTankLevel is accurate after attempting to drive past tank range
+        [TestMethod]
+        public void TestGasTankAfterExceedingTankRange()
+        {
+            test_car.Drive(600);
+            Assert.AreEqual(0, test_car.GasTankLevel, .001);
+        }
+
+        //TODO: can't have more gas than tank size, expect an exception
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGasOverfillException()
+        {
+           test_car.AddGas(5);
+           Assert.Fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
         }
     }
 }
